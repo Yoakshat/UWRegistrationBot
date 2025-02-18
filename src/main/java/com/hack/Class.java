fbab9ae9c141b42bd2f8389bcb09dad3735b5056
Yoakshat <51726869+Yoakshat@users.noreply.github.com>
2025-02-18 15:00:30 -0800

    Incorporated Quiz Sections!

--- src/main/java/com/hack/Class.java
+++ src/main/java/com/hack/Class.java
@@ -5,48 +5,75 @@ import java.util.*;
 public class Class implements Comparable<Class> {
     
     private String name; 
-    private List<Lecture> lectures; 
+    // everything is a type of quiz section
+    private List<QuizSection> sections; 
 
-    public Class(String name, List<Lecture> lectures){
+    public Class(String name, List<QuizSection> sections){
         this.name = name; 
-        this.lectures = lectures; 
+        this.sections = sections;
     }
 
-    public List<LecMod> removeOverlap(Lecture picked){
+    public List<LecMod> removeOverlap(QuizSection picked){
         // we want a way to store (index, object)
         // we have to store two arrays -> toRemove, and index
 
-        List<Lecture> toRemove = new ArrayList<>();
-        List<LecMod> store = new ArrayList<>();
+        List<QuizSection> toRemove = new ArrayList<>();
+        List<LecMod> addBack = new ArrayList<>(); 
 
         int i = 0; 
-        for(Lecture lecture: lectures){
-            if(lecture.overlaps(picked)){
-                // remove lecture
-                toRemove.add(lecture);
-                store.add(new LecMod(i, lecture));
-            } 
+    
+        
+        for(QuizSection section: sections){
+            SecMod lecOver = null; 
+            QuizMod quizOver = null; 
+
+            // quiz removeOverlap
+            if(!section.isQuiz()){
+                Lecture sec = (Lecture)(section);
+                // should return List<QuizMod> 
+                quizOver = sec.removeQuizOverlap(picked);
+            }
             
+            // section removeOverlap
+            if(section.overlaps(picked)){
+                // remove lecture
+                toRemove.add(section);
+                // create a sectionMod
+                lecOver = new SecMod(i, section);
+            } 
+
+            // order of above doesn't actually matter bcoz of reference semantics
+            addBack.add(new LecMod(lecOver, quizOver));
+
             i += 1; 
         }
 
-        lectures.removeAll(toRemove);
-        return store;
+        return addBack;
     }
 
-    public void addBack(List<LecMod> deleted){
-        for(LecMod del: deleted){
-            lectures.add(del.i, del.lec);
+    // given the List of LecMods, add back
+    public void addOverlap(List<LecMod> lecs){
+        
+        for(LecMod lec: lecs){
+            // add back to sections
+            if(lec.section != null){
+                lec.addSec(sections);
+            }
+
+            // add back to quiz sections
+            if(lec.quizSecs != null){
+                // tell quizMod to add it back
+                lec.addQuiz();
+            }
         }
     }
 
-    // build a compareTo to sort classes later
-    public Lecture get(int i){
-        return lectures.get(i);
+    public QuizSection get(int i){
+        return sections.get(i);
     }
 
-    public int numLectures(){
-        return lectures.size();
+    public int numSections(){
+        return sections.size();
     }
 
     @Override
@@ -57,7 +84,7 @@ public class Class implements Comparable<Class> {
     // will be ascending order 
     @Override
     public int compareTo(Class other) {
-        return this.numLectures() - other.numLectures();
+        return this.numSections() - other.numSections();
     }
 
 
---
+++ src/main/java/com/hack/SecMod.java
@@ -0,0 +1,13 @@
+package com.hack;
+
+// the only point of the class is to help unchoose and choose
+public class SecMod {
+    public int i; 
+    public QuizSection sec; 
+
+    public SecMod(int i, QuizSection sec){
+        this.i = i; 
+        this.sec = sec; 
+    }
+    
+}
